7fafa22ecff54669fe976bb81081a13f870f6846
Abdullah Alnazawi <abdull_aljuhine@hotmail.com>
2026-08-14 04:13:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,5 +44,8 @@ client.on('messageCreate', async (message) => {
     } catch (e) {}
   }, delay);
 });
+// Keep Render happy
+const http = require('http');
+http.createServer((req, res) => res.end('Bot is running!')).listen(process.env.PORT || 3000);
 
 client.login(config.token);
